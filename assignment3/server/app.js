7c1995b8f69a8977677362a3ab9186b157e07620
sivakumar178107 <hemanthgara.hg@gmail.com>
2023-04-16 18:31:03 +0530

    added new code

--- assignment3/server/app.js
+++ assignment3/server/app.js
@@ -36,10 +36,14 @@ const userSchema = new mongoose.Schema({
 
 const User=mongoose.model('User',userSchema);
 const Item = mongoose.model('Item', itemSchema);
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://52.202.201.124:27017/mydatabase', { //Change this line to the MongoDB Docker container URL
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -107,10 +111,30 @@ app.get('/api/items', async (req, res) => {
 });
 // Update an item in the database by id
 // Update an item in the database by id
+// app.delete('/api/items/:id', async (req, res) => {
+//   const itemId = req.params.id;
+//   try {
+//     const itemToDelete = await Item.findByIdAndDelete(itemId);
+//     if (!itemToDelete) {
+//       return res.status(404).send({ error: 'Item not found' });
+//     }
+//     const s3Params = {
+//       Bucket: process.env.AWS_BUCKET_NAME,
+//       Key: `uploads/${itemToDelete.itemImage.split('/').pop()}`
+//     };
+//     console.log(`uploads/${itemToDelete.itemImage.split('/').pop()}`)
+//     // await s3.deleteObject(s3Params).promise();
+//     await itemToDelete.remove();
+//     res.send({ message: 'Item deleted successfully' });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send(err);
+//   }
+// });
 app.delete('/api/items/:id', async (req, res) => {
   const itemId = req.params.id;
   try {
-    const itemToDelete = await Item.findById(itemId);
+    const itemToDelete = await Item.findByIdAndDelete(itemId);
     if (!itemToDelete) {
       return res.status(404).send({ error: 'Item not found' });
     }
@@ -119,8 +143,7 @@ app.delete('/api/items/:id', async (req, res) => {
       Key: `uploads/${itemToDelete.itemImage.split('/').pop()}`
     };
     console.log(`uploads/${itemToDelete.itemImage.split('/').pop()}`)
-    await s3.deleteObject(s3Params).promise();
-    await itemToDelete.remove();
+    // await s3.deleteObject(s3Params).promise();
     res.send({ message: 'Item deleted successfully' });
   } catch (err) {
     console.error(err.message);
